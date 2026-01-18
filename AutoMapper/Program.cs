e56634c9bab4359bcb71cc6f08ba688379e2d9fe
PawXQ <pawxq@mail.com>
2026-01-18 23:18:15 +0800

    Add project files.

---
+++ AutoMapper/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cardModel = new CardModel();
+            var data = Mapper.Map<CardViewModel>(cardModel);
+        }
+    }
+}
